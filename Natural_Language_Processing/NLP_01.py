e56886c9c0204e14fde0907c61151b8dcf808e7d
Nihara Padil <niharapadilhr@gmail.com>
2024-12-04 11:31:50 +0530

    Update NLP_01.py

--- Natural_Language_Processing/NLP_01.py
+++ Natural_Language_Processing/NLP_01.py
@@ -4,44 +4,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.data import find
-def download_nltk_data():
-    try:
-        find('corpora/stopwords.zip')
-        find('tokenizers/punkt.zip')
-    except LookupErroe:
-        nltk.download('stopwords')
-        nltk.download('punkt')
-def tokenize(text):
-    return word_tokenize(text)
-def filter_text(tokens):
-    return [re.sub(r'[^a-zA-Z0-9]', '',token) for token in token if token]
-def validate_script(tokens):
-    return[token for token in tokens if re.match(r'^[a-zA-Z]+$',token)]
-def remove_stop_words(tokens):
-    stop_words=set(stopwords.word('english'))
-    return [token for token in tokens if token.lower()not in stop_words]
-def stem_tokens(tokens):
-    stemmer =PorterStemmer()
-    return [stemmer.stem(token) for token in tokens]
-def preprocess_text(text):
-    download_nltk_data()
-    tokens= tokenize(text)
-    tokens=filter_text(tokens)
-
-
-
-
-#------------
-
-
-
-
-import nltk
-import re
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-from nltk.data import find
 
 def download_nltk_data():
     try:
