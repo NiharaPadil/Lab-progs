fb492c1760f88729dce5e99598b46093e8f2a841
Nihara Padil <niharapadilhr@gmail.com>
2024-12-05 08:48:40 +0530

    Update NLP_01.py

--- Natural_Language_Processing/NLP_01.py
+++ Natural_Language_Processing/NLP_01.py
@@ -44,3 +44,30 @@ if __name__ == "__main__":
    
     processed_tokens = preprocess_text(sample_text)
     print("Processed Tokens:", processed_tokens)
+
+
+
+
+
+#or 
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def pre_process(text):
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.lower()  not in stop_words]
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(word) for word in tokens]
+    return tokens
+
+text = "Neural Language Processing(NLP) is a field of artificial intellignece that focuses on"
+preprocessed_text = pre_process(text)
+print("Preprocessed Text is: ", preprocessed_text)
+
