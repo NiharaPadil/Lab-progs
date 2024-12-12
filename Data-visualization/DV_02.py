dde38b4a498f22e1b51d650dc9e98abd8b6b2e9e
Nihara Padil <niharapadilhr@gmail.com>
2024-12-12 20:43:03 +0530

    Update DV_02.py

--- Data-visualization/DV_02.py
+++ Data-visualization/DV_02.py
@@ -27,66 +27,74 @@ plt.grid()
 plt.legend()
 plt.show()
 
+#2a shorten code 
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# files = ['GOOGL_data.csv', 'FB_data.csv', 'AAPL_data.csv', 'AMZN_data.csv', 'MSFT_data.csv']
+# labels = ['Google', 'Facebook', 'Apple', 'Amazon', 'Microsoft']
+# data = [pd.read_csv(file).assign(date=lambda df: pd.to_datetime(df['date'])) for file in files]
+
+# plt.figure(figsize=(16, 8), dpi=300)
+# for df, label in zip(data, labels):
+#     plt.plot(df['date'], df['close'], label=label)
+
+# plt.xticks(rotation=70)
+# plt.yticks(range(0, 1450, 100))
+# plt.title('Stock Trend', fontsize=16)
+# plt.ylabel('Closing Price in $', fontsize=14)
+# plt.grid()
+# plt.legend()
+# plt.show()
+
 
 
 #2b
-
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-%matplotlib inline
+import numpy as np
 
 movie_scores = pd.read_csv('movie_scores.csv')
 
 plt.figure(figsize=(10, 5), dpi=300)
-pos = np.arange(len(movie_scores['MovieTitle']))
+pos = np.arange(len(movie_scores))
 width = 0.3
+
 plt.bar(pos - width / 2, movie_scores['Tomatometer'], width, label='Tomatometer')
 plt.bar(pos + width / 2, movie_scores['AudienceScore'], width, label='Audience Score')
-plt.xticks(pos, rotation=10)
-plt.yticks(np.arange(0, 101, 20))
 
-ax = plt.gca()
-ax.set_xticklabels(movie_scores['MovieTitle'])
-ax.set_yticklabels(['0%', '20%', '40%', '60%', '80%', '100%'])
-ax.set_yticks(np.arange(0, 100, 5), minor=True)
-ax.yaxis.grid(which='major')
-ax.yaxis.grid(which='minor', linestyle='--')
+plt.xticks(pos, movie_scores['MovieTitle'], rotation=10)
+plt.yticks(np.arange(0, 101, 20), labels=['0%', '20%', '40%', '60%', '80%', '100%'])
+plt.grid(axis='y', which='both', linestyle='--')
 
-plt.title('Movie comparison')
+plt.title('Movie Comparison')
 plt.legend()
 plt.show()
 
 
 
+
 #2c
-import pandas as sb
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-%matplotlib inline
 
 bills = sns.load_dataset('tips')
 days = ['Thur', 'Fri', 'Sat', 'Sun']
-days_range = np.arange(len(days))
-smoker = ['Yes', 'No']
-bills_by_days = [bills[bills['day'] == day] for day in days]
-bills_by_days_smoker = [[bills_by_days[day][bills_by_days[day]['smoker'] == s] for s in smoker] for day in days_range]
-total_by_days_smoker = [[bills_by_days_smoker[day][s]['total_bill'].sum() for s in range(len(smoker))] for day in days_range]
-totals = np.asarray(total_by_days_smoker)
+totals = np.array([
+    [bills[(bills['day'] == day) & (bills['smoker'] == smoker)]['total_bill'].sum() for smoker in ['Yes', 'No']]
+    for day in days
+])
 
 plt.figure(figsize=(10, 5), dpi=300)
-plt.bar(days_range, totals[:, 0], label='Smoker')
-plt.bar(days_range, totals[:, 1], bottom=totals[:, 0], label='Non-smoker')
+plt.bar(range(len(days)), totals[:, 0], label='Smoker')
+plt.bar(range(len(days)), totals[:, 1], bottom=totals[:, 0], label='Non-smoker')
+
+plt.xticks(range(len(days)), days)
+plt.ylabel('Daily Total Sales in $')
+plt.title('Restaurant Performance')
+plt.grid(axis='y')
 plt.legend()
-plt.xticks(days_range)
-ax = plt.gca()
-ax.set_xticklabels(days)
-ax.yaxis.grid()
-plt.ylabel('Daily total sales in $')
-plt.title('Restaurant performance')
 plt.show()
 
 
