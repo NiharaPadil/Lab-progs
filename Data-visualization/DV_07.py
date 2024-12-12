fa189486a1d806d86eda43733ad67447badd49c7
Nihara Padil <niharapadilhr@gmail.com>
2024-12-12 21:00:37 +0530

    Update DV_07.py

--- Data-visualization/DV_07.py
+++ Data-visualization/DV_07.py
@@ -1,40 +1,25 @@
 # a)
 import folium
-my_map1 = folium.Map(location = [28.5011226, 77.4099794],zoom_start = 12 )
-my_map1.save(" my_map1.html ")
-
-
-
+my_map1 = folium.Map(location=[28.5011226, 77.4099794], zoom_start=12)
+my_map1.save("my_map1.html")
 
 # b)
-import folium
-my_map2 = folium.Map(location = [28.5011226, 77.4099794],zoom_start = 12)
-folium.CircleMarker(location = [28.5011226, 77.4099794],radius = 50, popup = ' FRI ').add_to(my_map2)
-my_map2.save(" my_map2.html ")
-
-
+my_map2 = folium.Map(location=[28.5011226, 77.4099794], zoom_start=12)
+folium.CircleMarker(location=[28.5011226, 77.4099794], radius=50, popup='FRI').add_to(my_map2)
+my_map2.save("my_map2.html")
 
 # c)
-import folium
-my_map3 = folium.Map(location = [28.5011226, 77.4099794],zoom_start = 15)
-folium.Marker([28.5011226, 77.4099794],popup = ' Geeksforgeeks.org ').add_to(my_map3)
-my_map3.save(" my_map3.html ")
+my_map3 = folium.Map(location=[28.5011226, 77.4099794], zoom_start=15)
+folium.Marker([28.5011226, 77.4099794], popup='GeeksforGeeks.org').add_to(my_map3)
+my_map3.save("my_map3.html")
 
-
-
-
-import folium
+# d)
 my_map4 = folium.Map(location=[28.5011226, 77.4099794], zoom_start=12)
 folium.Marker([28.704059, 77.102490], popup='Delhi').add_to(my_map4)
 folium.Marker([28.5011226, 77.4099794], popup='GeeksforGeeks').add_to(my_map4)
 folium.PolyLine(locations=[(28.704059, 77.102490), (28.5011226, 77.4099794)], line_opacity=0.5).add_to(my_map4)
 my_map4.save("my_map4.html")
 
-
-
-
-
-
-
+# Print current working directory
 import os
-print(os.getcwd())  
+print(os.getcwd())
